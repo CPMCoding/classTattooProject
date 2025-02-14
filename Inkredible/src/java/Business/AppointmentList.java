3626c29eeabdd727b0201483e16f34d4870b6893
Tstone6663 <tstone160@gmail.com>
2025-02-14 18:39:19 -0500

    CustomerLogin Servlet Code & AppointmentList class added
    Went ahead and got the login servlet for customers finished so you should be good to login as a customer. Also, I finished setting up the AppointmentList class which will allow you to show multiple appointments. Will be used primarily for Admins and Artists.

---
+++ Inkredible/src/java/Business/AppointmentList.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author tston
+ */
+public class AppointmentList {
+    public int count;
+    public Appointments accArr[] = new Appointments[10];
+    
+    public void addAppointment (Appointments al){
+        accArr[count] = al;
+        count++;
+    }
+    
+    public void displayList(){
+        for(int i = 0; i < count; i++){
+            accArr[i].display();
+        }
+    }
+    
+    public static void main(String[] args){
+        
+    }
+}
--- Inkredible/src/java/Servlets/CustomerLogin.java
+++ Inkredible/src/java/Servlets/CustomerLogin.java
@@ -2,14 +2,17 @@
  * The servlet we will use when a Customer wants to login. 
  */
 package Servlets;
-
+import Business.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,18 +33,40 @@ public class CustomerLogin extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CustomerLogin</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CustomerLogin at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        
+        String custID, custPass;
+        custID = request.getParameter("cID"); //depends on ID in JSP page, may change from cID
+        custPass = request.getParameter("pass"); //depends on ID in JSP page, may change from pass
+        
+        try{
+            Customers c1 = new Customers();
+            c1.selectDB(custID);
+            String pwdb = c1.getCPW();
+            
+            Appointments a1 = new Appointments();
+            a1.selectCustDB(custID);
+            
+            HttpSession ses1;
+            ses1 = request.getSession();
+            ses1.setAttribute("c1", c1); //Telling the sesion what the attribute is for the customer class
+            ses1.setAttribute("a1", a1); //Telling the session what the attribute is for the Appointments class 
+            System.out.println("Customer added to session");
+            
+            if(custPass.equals(pwdb)){
+                RequestDispatcher rd = request.getRequestDispatcher("CustomerHomePage.jsp"); //Temp name "CustomerHomePage.jsp" may change
+                rd.forward(request,response);
+            }
+            else{
+                RequestDispatcher rd = request.getRequestDispatcher("LoginError.jsp"); //Temp name "LoginError.jsp" may change
+                rd.forward(request,response);
+            }
+            
         }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
